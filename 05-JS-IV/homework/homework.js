1e8dd47933f0f65d6c58055027865f4df558afcb
loreperez <lorenavperez10@gmail.com>
2022-02-01 22:16:26 -0300

    JS 05 Terminado

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,12 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var gato = {
+    nombre: nombre,
+    edad: edad,
+    meow: function(){return "Meow!"}
+  }
+  return gato
 }
 
 
@@ -14,6 +20,8 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto[property] = null
+  return objeto
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -21,6 +29,7 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
